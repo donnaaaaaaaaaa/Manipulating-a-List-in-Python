6e7293063c8c7f6c7177e444358f34dd674aea67
donnaaaaaaaaaa <bacanidonnamae452@gmail.com>
2024-10-18 17:03:28 +0800

    Add files via upload

---
+++ list/slicing.py
@@ -0,0 +1,3 @@
+fruits = ['apple', 'banana', 'cherry', 'mango', 'strawberry']
+print(fruits[1:]) # Output: ['banana', 'cherry', 'mango', 'strawberry']
+print(fruits[:3]) # Output: ['apple', 'banana', 'cherry']
